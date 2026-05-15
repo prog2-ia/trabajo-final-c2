de10068ff2b6ee7ad3575fa8d578c75ab5ef0dd1
Nicolas Sánchez Saura <168089703+nic0310@users.noreply.github.com>
2026-05-15 13:25:51 +0200

    hecho __init__.py de reglas

---
+++ src/reglas/__init__.py
@@ -0,0 +1,4 @@
+from reglas.reglaHabito import ReglaHabito
+from reglas.regla_frecuencia import ReglaFrecuencia
+from reglas.regla_rango import ReglaRango
+from reglas.regla_umbral import ReglaUmbral
